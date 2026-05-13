649a5702e726ebceabb0e93bc16e5e1321f8ac2a
HieuPNT <79359537+phamhieuit04@users.noreply.github.com>
2026-05-14 00:00:43 +0700

    Add user & setting entity

---
+++ app/src/main/java/com/example/vibramobile/data/source/local/entity/UserEntity.kt
@@ -0,0 +1,27 @@
+package com.example.vibramobile.data.source.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class UserEntity(
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: Int? = null,
+
+    @ColumnInfo(name = "name")
+    val name: String? = null,
+
+    @ColumnInfo(name = "description")
+    val description: String? = null,
+
+    @ColumnInfo(name = "email")
+    val email: String? = null,
+
+    @ColumnInfo(name = "avatar_path")
+    val avatarPath: String? = null,
+
+    @ColumnInfo(name = "token")
+    val token: String? = null
+)
